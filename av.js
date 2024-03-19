18fad839d73bb8e8cf40f21146b1d3f71974e8e6
phucle996 <163315305+phucle996@users.noreply.github.com>
2024-03-19 14:57:24 +0700

    Update av.js

--- av.js
+++ av.js
@@ -1,14 +1,21 @@
+// Import module 'http'
 const http = require('http');
 
+// Define hostname và port
 const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+const port = 3000;
 
+// Tạo một máy chủ HTTP đơn giản
 const server = http.createServer((req, res) => {
+  // Ghi header response
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  // Gửi nội dung 'Hello World' khi có request
   res.end('Hello World\n');
 });
 
+// Lắng nghe trên cổng đã xác định
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
